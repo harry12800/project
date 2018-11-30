2d134f3aa4efc0cc150992127779b574d6a7fe0d
wshua <sling@wshua.cc>
2018-11-30 11:08:48 +0800

    1

--- vchatserver/src/main/java/cn/harry12800/vchatserver/App.java
+++ vchatserver/src/main/java/cn/harry12800/vchatserver/App.java
@@ -1,4 +1,4 @@
-package cn.harry12800.db;
+package cn.harry12800.vchatserver;
 
 /**
  * Hello world!
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-    	
+        System.out.println( "Hello World!" );
     }
 }
