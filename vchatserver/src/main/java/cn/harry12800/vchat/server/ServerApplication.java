72c2fac7baa22b9bfbf8f8e8b0dccd9825065dca
harry12800 <804151219@qq.com>
2018-12-20 23:10:41 +0800

    ‘

---
+++ vchatserver/src/main/java/cn/harry12800/vchat/server/ServerApplication.java
@@ -0,0 +1,19 @@
+package cn.harry12800.vchat.server;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+@SpringBootApplication
+@ComponentScan({"cn.harry12800"})
+@MapperScan("cn.harry12800.db.mapper")
+public class ServerApplication {
+
+	public static void main(String[] args) {
+		ConfigurableApplicationContext run = SpringApplication.run(ServerApplication.class, args);
+		Server server = run.getBean(Server.class);
+		server.start();
+	}
+}
