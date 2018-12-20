04c33f1ef26f63d949fd94f63e6b287f742d3f85
harry12800 <804151219@qq.com>
2018-12-21 00:33:51 +0800

    1

---
+++ vchatserver/src/main/java/cn/harry12800/vchat/server/server/Server.java
@@ -0,0 +1,49 @@
+package cn.harry12800.vchat.server.server;
+
+import org.springframework.stereotype.Component;
+
+import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.nio.NioServerSocketChannel;
+
+/**
+ * netty服务端
+ * 
+ * @author harry12800
+ * 
+ */
+@Component
+public class Server {
+
+	/**
+	 * 启动
+	 */
+	public void start() {
+
+		// 服务类
+		ServerBootstrap b = new ServerBootstrap();
+		// 创建boss和worker
+		EventLoopGroup bossGroup = new NioEventLoopGroup();
+		EventLoopGroup workerGroup = new NioEventLoopGroup();
+		try {
+			// 设置循环线程组事例
+			b.group(bossGroup, workerGroup);
+			// 设置channel工厂
+			b.channel(NioServerSocketChannel.class);
+
+			// 设置管道
+			b.childHandler(new ServerChannelInitializer());
+			b.option(ChannelOption.SO_BACKLOG, 2048);// 链接缓冲池队列大小
+
+			// 绑定端口
+			b.bind(12800).sync();
+
+			System.out.println("start!!!");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
