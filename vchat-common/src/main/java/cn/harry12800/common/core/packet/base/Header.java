74f51742012b6922793af8996e87849f4e45cc54
harry12800 <804151219@qq.com>
2018-12-21 10:44:14 +0800

    1

--- vchat-common/src/main/java/cn/harry12800/common/core/packet/base/Header.java
+++ vchat-common/src/main/java/cn/harry12800/common/core/packet/base/Header.java
@@ -7,13 +7,13 @@ public class Header {
 	public short version=1; // 版本号
 	@Tag(2)
 	public short dataType=0; // 群消息，还是单聊消息，其他消息
-	@Tag(4)
-	public long destination=0; // 目的地
 	@Tag(3)
-	public int serviceId=0; // SID
+	public long destination=0; // 目的地
 	@Tag(4)
-	public int commandId=0; // CID
+	public int serviceId=0; // SID
 	@Tag(5)
+	public int commandId=0; // CID
+	@Tag(6)
 	public short reserved=0; // 保留，可用于如序列号等
 
 	public Header() { }
